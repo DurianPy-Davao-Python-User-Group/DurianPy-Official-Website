6a68e9f66c762141e666d4d9606ff77de2d4c6d1
44mira <legolaslada@gmail.com>
2025-02-20 18:40:00 +0800

    test: add test route for carousel

---
+++ app/test/page.tsx
@@ -0,0 +1,27 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../components/ui/carousel';
+
+export default function TestCarousel() {
+  return (
+    <div className='flex justify-center'>
+      <Carousel
+        opts={{ align: "start" }}
+        className='w-full max-w-sm'>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} >
+              <div className='p-1 text-center'>{index}</div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
