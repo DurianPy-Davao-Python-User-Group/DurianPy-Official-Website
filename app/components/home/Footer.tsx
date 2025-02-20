245e9270bb4bade9ed6833f9c5661fa97ae54e80
Aaron Dave Siapuatco <aarondavesiapuatco@Aarons-MacBook-Air.local>
2025-02-20 21:34:05 +0800

    chore: pushed latest changes

--- app/components/home/Footer.tsx
+++ app/components/home/Footer.tsx
@@ -1,29 +1,37 @@
 import { Container } from '../ui/Container';
-
+import Image from 'next/image';
+import logo from '../../assets/images/logo.svg';
 export function Footer() {
   return (
     <div className="bg-dark-green">
-      <footer className="bg-gradient-utd-saturatedGreen-transparent rounded-t-[100px] pt-[128px] ">
+      <footer className="bg-gradient-utd-saturatedGreen-transparent rounded-t-[100px] pt-16 ">
         <Container>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12 border-b border-primary pb-8">
-            <div className="relative pr-6 after:absolute after:top-2 after:bottom-2 after:right-0 after:w-[1px]  after:bg-primary md:after:block after:hidden">
-              <h3 className="text-white font-bold mb-4">DurianPy</h3>
-              <p className="text-green-100">
-                Accelerating Davao&apos;s Tech Growth with Python
-              </p>
+          <div className="grid grid-cols-1 md:grid-cols-[25%_40%_30%] gap-8 mb-12 border-b border-primary ">
+            <div className="relative after:absolute after:top-12 after:bottom-8 after:right-0 after:w-[1px]  after:bg-primary md:after:block after:hidden min-h-[250px]">
+              <div className="max-w-[230px] -mt-4">
+                <Image src={logo} alt="DurianPy Logo" width={220} />
+                <p className="text-green-100 text-sm pl-2 -mt-4">
+                  Accelerating{' '}
+                  <span className="text-primary">Davao&apos;s </span>
+                  Tech Growth with Python
+                </p>
+                {/* TODO: Add button with after init shadcn issue */}
+              </div>
             </div>
 
-            <div className="relative pr-6 after:absolute after:top-2 after:bottom-2 after:right-0 after:w-[1px] after:bg-primary md:after:block after:hidden">
-              <h4 className="text-white font-bold mb-4">Community</h4>
+            <div className="relative p-12 -mt-4 after:absolute after:top-12 after:bottom-8 after:right-0 after:w-[1px] after:bg-primary md:after:block after:hidden">
+              <h4 className="text-primary text-lg font-black mb-4">
+                Newsletter
+              </h4>
               <ul className="space-y-2">
                 <li>
                   <a href="#" className="text-green-100 hover:text-white">
-                    Events
+                    Email
                   </a>
                 </li>
                 <li>
                   <a href="#" className="text-green-100 hover:text-white">
-                    Join Us
+                    ___________
                   </a>
                 </li>
                 <li>
@@ -34,8 +42,8 @@ export function Footer() {
               </ul>
             </div>
 
-            <div>
-              <h4 className="text-white font-bold mb-4">Resources</h4>
+            <div className="pt-6">
+              <h4 className="text-white font-bold mb-4">Navigation</h4>
               <ul className="space-y-2">
                 <li>
                   <a href="#" className="text-green-100 hover:text-white">
