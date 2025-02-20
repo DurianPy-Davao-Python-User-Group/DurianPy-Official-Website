7c4fbf0975e7d10edf19916ceb64cab04c703db4
44mira <legolaslada@gmail.com>
2025-02-20 18:43:29 +0800

    format: test/page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -8,14 +8,12 @@ import {
 
 export default function TestCarousel() {
   return (
-    <div className='flex justify-center'>
-      <Carousel
-        opts={{ align: "start" }}
-        className='w-full max-w-sm'>
+    <div className="flex justify-center">
+      <Carousel opts={{ align: 'start' }} className="w-full max-w-sm">
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} >
-              <div className='p-1 text-center'>{index}</div>
+            <CarouselItem key={index}>
+              <div className="p-1 text-center">{index}</div>
             </CarouselItem>
           ))}
         </CarouselContent>
