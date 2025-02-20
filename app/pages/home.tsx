2d59d39200cf15db37353710a9075addde816aaf
Sam Dacara <128273192+Samshh@users.noreply.github.com>
2025-02-20 08:48:08 +0800

    Revert "added tailwind config based on figma colors"

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -9,61 +9,6 @@ import { Footer } from '../components/home/Footer';
 export default function HomePage() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-4 p-4 bg-red-500">
-        <div className="h-32 w-32 bg-primary text-black flex text-center justify-center">
-          primary
-        </div>
-        <div className="h-32 w-32 bg-dark-green text-white flex text-center justify-center">
-          dark-green
-        </div>
-        <div className="h-32 w-32 bg-white text-black flex text-center justify-center border">
-          white
-        </div>
-        <div className="h-32 w-32 bg-saturated-light-green text-black flex text-center justify-center">
-          saturated-light-green
-        </div>
-        <div className="h-32 w-32 bg-light-dark-green text-white flex text-center justify-center">
-          light-dark-green
-        </div>
-        <div className="h-32 w-32 bg-medium-dark-green text-white flex text-center justify-center">
-          medium-dark-green
-        </div>
-        <div className="h-32 w-32 bg-black-30-opacity text-white flex text-center justify-center">
-          black-30-opacity
-        </div>
-
-        {/* Background Images */}
-        <div className="h-32 w-32 bg-gradient-ltr-lightgreen-transparent text-black flex text-center justify-center">
-          ltr-lightgreen-transparent
-        </div>
-        <div className="h-32 w-32 bg-gradient-utd-lightgreen-darkgreen text-white flex text-center justify-center">
-          utd-lightgreen-darkgreen
-        </div>
-        <div className="h-32 w-32 bg-gradient-ltr-darkgreen-lightgreen text-white flex text-center justify-center">
-          ltr-darkgreen-lightgreen
-        </div>
-        <div className="h-32 w-32 bg-gradient-ltr-solid-yellow-lightyellow text-black flex text-center justify-center">
-          ltr-solid-yellow-lightyellow
-        </div>
-        <div className="h-32 w-32 bg-gradient-ltr-transparent-yellow text-black flex text-center justify-center">
-          ltr-transparent-yellow
-        </div>
-        <div className="h-32 w-32 bg-gradient-utd-transparent-black text-white flex text-center justify-center">
-          utd-transparent-black
-        </div>
-        <div className="h-32 w-32 bg-gradient-middle-green-transparent text-white flex text-center justify-center">
-          middle-green-transparent
-        </div>
-        <div className="h-32 w-32 bg-gradient-ltr-green-transparent text-white flex text-center justify-center">
-          ltr-green-transparent
-        </div>
-        <div className="h-32 w-32 bg-gradient-utd-saturatedGreen-transparent text-black flex text-center justify-center">
-          utp-saturatedGreen-transparent
-        </div>
-        <div className="h-32 w-32 bg-gradient-utd-green-transparent text-white flex text-center justify-center border">
-          utd-green-transparent
-        </div>
-      </div>
       <Hero />
       <Carousel />
       <CTASection />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,42 +11,6 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
-        //Solid Colors
-
-        primary: '#FFC200', //Yellow
-        'dark-green': '#112018', //Dark Green (used for bg)
-        white: '#FFFFFF',
-        'saturated-light-green': '#00FF00', //light green
-        'light-dark-green': '#00401C', //light green (used in outline)
-        'medium-dark-green': '#1A3E2A', //medium dark green
-        'black-30-opacity': 'rgba(0, 0, 0, 0.3)' , //Use for overlay in pictures to darken
-      },
-      backgroundImage: {
-        // Always double-check if opacity adjustments were applied to the gradient or overall color appearance. 
-        // Sometimes, gradients are visually manipulated (e.g., in Figma) to achieve a specific look, 
-        // which may not directly translate to Tailwind CSS. Ensure the intended effect is accurately recreated.
-
-        'gradient-ltr-lightgreen-transparent'://linear fading gradient
-          'linear-gradient(to right, #36FF90,rgba(115, 115, 115, 0))',
-        'gradient-utd-lightgreen-darkgreen'://100% opaque
-          'linear-gradient(to bottom, #1C653B, #113620)', 
-        'gradient-ltr-darkgreen-lightgreen'://100% opaque
-          'linear-gradient(to right, #1A3E2A, #3EB372)',
-        'gradient-ltr-solid-yellow-lightyellow'://100% opaque
-          'linear-gradient(to right, #FFC200, #FFDC6C)',
-        'gradient-ltr-transparent-yellow'://linear fading gradient
-          'linear-gradient(to right,rgba(138, 255, 191, 0), #FFC201)',//BG in Recognition section
-        'gradient-utd-transparent-black'://linear fading gradient
-          'linear-gradient(180deg, rgba(102, 102, 102, 0) 0%, rgba(0, 0, 0, 0.49) 18%, rgba(0, 0, 0, 1) 48%)', //BG in Recognition section
-        'gradient-middle-green-transparent'://middle vertical fading gradient
-          'linear-gradient(180deg, rgba(17,32,24,0) 0%, rgba(17,32,24,0.66) 23%, rgba(17,32,24,1) 44%, rgba(17,32,24,1) 66%, rgba(17,32,24,0) 100%)',
-        'gradient-ltr-green-transparent'://linear fading gradient
-          'linear-gradient(to right, #1A3E2A,rgba(26, 62, 42, 0))',
-        'gradient-utd-saturatedGreen-transparent'://linear fading gradient
-          'linear-gradient(to bottom, rgba(62, 179, 115, 0), #00833A)',
-        'gradient-utd-green-transparent': //linear fading gradient
-          'linear-gradient(180deg, rgba(17,32,24,0.8) ,rgba(0,64,28,1))',
-
       },
     },
   },
