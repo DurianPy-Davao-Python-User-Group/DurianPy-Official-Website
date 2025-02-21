49bc2acf78727ea0ee6fbd12920843c437a5aa64
Jodi <jodi.gavano@gmail.com>
2025-02-22 03:42:14 +0800

    Updated mobile section

--- app/components/home/Partners.tsx
+++ app/components/home/Partners.tsx
@@ -11,16 +11,56 @@ interface PartnersProps {
 
 export function Partners() {
   const partners: PartnersProps[] = [
-    { name: 'ADDVentures', logo: '/partner-logos/addventures-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'DEVCON Davao', logo: '/partner-logos/devcon-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'IDEAS Davao', logo: '/partner-logos/ideas-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'ACCESS', logo: '/partner-logos/access-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'AWSUG Davao', logo: '/partner-logos/awsug-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'CSSEC', logo: '/partner-logos/cssec-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'DICE', logo: '/partner-logos/dice-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'PizzaPy', logo: '/partner-logos/pizzapy-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'SAMAHAN', logo: '/partner-logos/samahan-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
-    { name: 'SPARCS', logo: '/partner-logos/sparcs-logo.png', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis' },
+    {
+      name: 'ADDVentures',
+      logo: '/partner-logos/addventures-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'DEVCON Davao',
+      logo: '/partner-logos/devcon-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'IDEAS Davao',
+      logo: '/partner-logos/ideas-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'ACCESS',
+      logo: '/partner-logos/access-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'AWSUG Davao',
+      logo: '/partner-logos/awsug-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'CSSEC',
+      logo: '/partner-logos/cssec-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'DICE',
+      logo: '/partner-logos/dice-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'PizzaPy',
+      logo: '/partner-logos/pizzapy-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'SAMAHAN',
+      logo: '/partner-logos/samahan-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
+    {
+      name: 'SPARCS',
+      logo: '/partner-logos/sparcs-logo.png',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi fuga est illum vero sapiente facere? Quas itaque harum praesentium a ut nobis',
+    },
   ];
 
   return (
--- app/components/home/PartnersMobile.tsx
+++ app/components/home/PartnersMobile.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 interface PartnersProps {
   name: string;
@@ -10,11 +10,16 @@ interface PartnersProps {
 const PartnersMobile = ({ partners }: { partners: PartnersProps[] }) => {
   return (
     <>
-      <div className="text-center text-2xl font-semibold mb-2 text-[#ffc200]">Partners</div>
+      <div className="text-center text-2xl font-semibold mb-2 text-[#ffc200]">
+        Partners
+      </div>
       <div className="text-center text-xs mb-6 px-12 text-white">
-        We collaborate with like-minded organizations to {" "}
-        <span className="text-[#417659]">foster creativity, innovation, and growth.</span>
-        Explore our partnerships and see how we create meaningful impact together.
+        We collaborate with like-minded organizations to{' '}
+        <span className="text-[#417659]">
+          foster creativity, innovation, and growth.
+        </span>
+        Explore our partnerships and see how we create meaningful impact
+        together.
       </div>
 
       <section className="py-6">
@@ -26,8 +31,8 @@ const PartnersMobile = ({ partners }: { partners: PartnersProps[] }) => {
             >
               {/* Partner Logo */}
               <div className="w-full flex items-center justify-center h-[110px] mt-4">
-                <Image 
-                  src={partner.logo} 
+                <Image
+                  src={partner.logo}
                   alt={partner.name}
                   width={120}
                   height={120}
@@ -41,8 +46,12 @@ const PartnersMobile = ({ partners }: { partners: PartnersProps[] }) => {
               {/* Partner Name and Description */}
               <div className="relative w-full text-center flex flex-col justify-center py-2 px-3 text-sm font-medium text-[#ffc200] rounded-b-lg h-[120px]">
                 <div className="absolute inset-0 bg-gradient-to-b from-[#4c8d69e3] to-[#10201885] opacity-50 rounded-b-lg h-[130px]"></div>
-                <p className="relative text-lg font-bold leading-none mt-8 mb-2">{partner.name}</p>
-                <p className="relative text-white text-xs mt-1 px-4">{partner.desc}</p>
+                <p className="relative text-lg font-bold leading-none mt-8 mb-2">
+                  {partner.name}
+                </p>
+                <p className="relative text-white text-xs mt-1 px-4">
+                  {partner.desc}
+                </p>
               </div>
             </div>
           ))}
@@ -55,6 +64,5 @@ const PartnersMobile = ({ partners }: { partners: PartnersProps[] }) => {
     </>
   );
 };
-
 
 export default PartnersMobile;
