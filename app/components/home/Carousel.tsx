fb8ecb6556e91054012a174656047f576d780e07
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2025-02-26 01:10:56 +0800

    feat: fade text out upon carousel hover

--- app/components/home/Carousel.tsx
+++ app/components/home/Carousel.tsx
@@ -16,46 +16,42 @@ export function Carousel() {
   ];
 
   return (
-    <section className="">
-      <div className='max-w-7xl mx-auto px-0'>
-        <div className="relative">
-          {/* CAROUSEL */}
-          <CarouselContainer
-            opts={{ loop: true }}
-            autoplay={true}
-            autoplayInterval={3000}
-          >
-            <CarouselContent>
-              {photos.map((photo, idx) => (
-                <CarouselItem
-                  key={idx}
-                  className="flex items-center justify-center"
-                >
-                  <img
-                    src={'assets/carousel/' + photo.image}
-                    alt={photo.name}
-                    className="object-cover w-full h-full"
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselDots className="absolute bottom-4 left-1/2 -translate-x-1/2 text-2xl py-4 text" />
+    <section className="group relative max-w-7xl mx-auto px-0">
+      {/* CAROUSEL */}
+      <CarouselContainer
+        opts={{ loop: true }}
+        autoplay={true}
+        autoplayInterval={3000}
+      >
+        <CarouselContent>
+          {photos.map((photo, idx) => (
+            <CarouselItem
+              key={idx}
+              className="flex items-center justify-center"
+            >
+              <img
+                src={'assets/carousel/' + photo.image}
+                alt={photo.name}
+                className="object-cover w-full h-full"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselDots className="absolute bottom-4 left-1/2 -translate-x-1/2 text-2xl py-4 text" />
 
-            <CarouselPrevious className="ml-[10%]" />
-            <CarouselNext className="mr-[10%]" />
-          </CarouselContainer>
+        <CarouselPrevious className="ml-[10%]" />
+        <CarouselNext className="mr-[10%]" />
+      </CarouselContainer>
 
-          {/* TEXT OVERLAY */}
-          <div className="w-full absolute top-1/2 left-0 -translate-y-1/2 text-center text-white pointer-events-none">
-            <h2 className="text-8xl font-bold">
-              Davao <span className="text-primary">Python</span> Community
-            </h2>
-            <p>
-              where Pythonistas and enthusiasts gather to discuss trends and
-              share knowledge
-            </p>
-          </div>
-        </div>
+      {/* TEXT OVERLAY */}
+      <div className="w-full absolute top-1/2 left-0 -translate-y-1/2 text-center text-white pointer-events-none transition-opacity duration-500 group-hover:opacity-0">
+        <h2 className="text-8xl font-bold">
+          Davao <span className="text-primary">Python</span> Community
+        </h2>
+        <p>
+          where Pythonistas and enthusiasts gather to discuss trends and share
+          knowledge
+        </p>
       </div>
     </section>
   );
