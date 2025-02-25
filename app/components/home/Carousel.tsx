a20967d69845c93a7f8de14673bea9b7026404a4
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2025-02-25 23:23:13 +0800

    feat: added official photos

--- app/components/home/Carousel.tsx
+++ app/components/home/Carousel.tsx
@@ -10,15 +10,11 @@ import {
 } from '../ui/carousel';
 
 export function Carousel() {
-  // dummy data, please change pag may cdn na
   const photos = [
-    {
-      name: 'PizzaPy',
-      image:
-        'https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-6/476085642_647117237888690_5048259540079263909_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHI2GPJnderjLqZD_onTz_mhl8LobvLU8iGXwuhu8tTyM7eHSLuYoiIOe6Op5d0VKJHbzWWV-QlLwAoC8XevZ3g&_nc_ohc=Vq6trOsC1RsQ7kNvgFESvtF&_nc_oc=Adiv5KZDlvtgxuchvdkXqgT7TQOlV5aif3gCE2ZlbQwMz-LBPNelrYoDMZbwkjvL2qPy9JajR3WJKRXRlBhEFlxr&_nc_zt=23&_nc_ht=scontent.fdvo2-1.fna&_nc_gid=AxfAZaWmN95V1AdUFkQV3Ni&oh=00_AYCLRyoyPE8elh14toICFJM6CKypHE3dCQ9K9UITsqp6eQ&oe=67C23A67',
-    },
-    { name: 'DevCon', image: '/devcon-logo.svg' },
-    { name: 'Google', image: '/google-logo.svg' },
+    { name: 'pyconf 2024', image: '1.png' },
+    { name: 'meetup #1', image: '2.png' },
+    { name: 'meetup #2', image: '3.png' },
+    { name: 'meetup #3', image: '4.png' },
   ];
 
   return (
@@ -28,13 +24,13 @@ export function Carousel() {
           {/* CAROUSEL */}
           <CarouselContainer className="w-full bg-blue-500">
             <CarouselContent>
-              {photos.map((photo) => (
+              {photos.map((photo, idx) => (
                 <CarouselItem
-                  key={photo.name}
+                  key={idx}
                   className="bg-green-700 p-6 rounded-lg flex items-center justify-center w-full"
                 >
                   <img
-                    src={photo.image}
+                    src={'assets/carousel/' + photo.image}
                     alt={photo.name}
                     className="object-cover"
                   />
