d50a45a11479d7ccc881f62023221741fca2d0b7
devRaxx <rpcastaneros@up.edu.ph>
2025-02-26 12:41:50 +0800

    fix: fixed paragraph responsivness

--- app/components/home/StatsAndReviews.tsx
+++ app/components/home/StatsAndReviews.tsx
@@ -86,10 +86,7 @@ export function StatsAndReviews() {
   }, []);
 
   return (
-    <section
-      ref={sectionRef}
-      className="bg-dark-green text-white h-screen"
-    >
+    <section ref={sectionRef} className="bg-dark-green text-white h-screen">
       <Container>
         <div className="flex flex-col items-center px-8 xl:px-14 2xl:flex-row 2xl:px-10 2xl:justify-between">
           <div className="w-full">
@@ -102,7 +99,7 @@ export function StatsAndReviews() {
             </div>
 
             {/* Section Description */}
-            <div className="text-center 2xl:text-left xl:text-xl sm:space-x-13 pb-12 md:space-x-15 lg:mx-60 xl:mx-0 2xl:mr-32 lg:space-x-10 xl:space-x-15 2xl:space-x-10">
+            <div className="text-center 2xl:text-left xl:text-xl sm:space-x-13 pb-12 md:space-x-15 md:mx-36 lg:mx-60 xl:mx-64 2xl:mx-0 2xl:mr-32 lg:space-x-10 xl:space-x-15 2xl:space-x-10">
               <p className="text-white">
                 <span className="font-bold">DurianPy</span> is a community for
                 Python enthusiasts to learn, share, and connect through monthly
