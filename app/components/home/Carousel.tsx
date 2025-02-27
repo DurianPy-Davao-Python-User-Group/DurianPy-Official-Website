1fc6b1fe26c27f3a32f6d4ab29dfc0c9f9490c7b
nica <nica.dable@gmail.com>
2025-02-27 23:11:54 +0800

    fix: removed black, made dark green blend more to the bottom

--- app/components/home/Carousel.tsx
+++ app/components/home/Carousel.tsx
@@ -39,17 +39,33 @@ export function Carousel() {
                 width={1960}
                 height={1280}
               />
-              <div className="absolute inset-0 bg-gradient-to-r from-[#1A3E2A]/60 to-transparent duration-500 group-hover:opacity-20" />
-              <div className="absolute inset-0 bg-gradient-to-r from-[#1A3E2A]/60 to-transparent transform scale-x-[-1] duration-500 group-hover:opacity-20" />
-              <div className="absolute inset-0 bg-gradient-to-t from-black via-black/10 to-transparent opacity-50 duration-500 group-hover:opacity-20" />
+
+              <div className="absolute left-[-20px] inset-y-0 bg-gradient-to-r from-[#1A3E2A]/20 to-transparent duration-500" />
+              <div className="absolute right-[-20px] inset-y-0 bg-gradient-to-r from-[#1A3E2A]/20 to-transparent -scale-x-100 duration-500" />
             </CarouselItem>
           ))}
         </CarouselContent>
+
+        {/* DARK GREEN */}
+        <div className="absolute inset-0 bg-gradient-to-t from-[#102019] via-[#102019]/40 sm:via-[#102019]/90 md:via-[#102019]/20 to-transparent duration-500" />
+
         <CarouselDots className="absolute bottom-4 left-1/2 -translate-x-1/2 text-2xl py-4 text" />
         <CarouselPrevious className="ml-[5%] h-16 w-16 sm:w-20 sm:h-20" />
         <CarouselNext className="mr-[5%] h-16 w-16 sm:w-20 sm:h-20" />
       </CarouselContainer>
+
+      {/* WHITE GRADIENT */}
       <div className="absolute top-[-90px] left-0 w-full h-[300px] bg-gradient-to-b from-transparent via-white/100 via-[30%] to-transparent" />
+
+      {/* 
+      <div
+        className="absolute inset-0 h-[30%] w-full top-[-150px] blur-sm scale-x-[2]"
+        style={{
+          background: `linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 36.9%, #FFF 65.4%, rgba(255, 255, 255, 0.00) 100%)`,
+          transform: 'scaleY(-1)',
+        }}
+      /> */}
+
       {/* TEXT OVERLAY */}
       <div className="w-full flex flex-col items-center absolute top-1/2 left-0 -translate-y-1/2 text-center text-white pointer-events-none transition-opacity duration-500 group-hover:opacity-0">
         <h2 className="w-[90%] text-3xl sm:text-5xl md:text-6xl xl:text-[5rem] font-bold">
