ce3578e1ec6ed42303bdc36bea68f4a5dd28bc21
Samshh <samdacs2@gmail.com>
2025-02-28 20:15:12 +0800

    chore: removed extra text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HomePage from './pages/home';
 export default function Home() {
   return (
     <main>
-      <div className="bg-blue-400 text-[16rem] text-black">asd</div>
       <HomePage />
     </main>
   );
