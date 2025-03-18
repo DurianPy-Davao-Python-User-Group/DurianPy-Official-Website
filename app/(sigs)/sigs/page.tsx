e4be61a0beaa76138e45e7d246356f015ed55ac9
devRaxx <rpcastaneros@up.edu.ph>
2025-03-18 16:45:33 +0800

    feat: initial commit + route setup

---
+++ app/(sigs)/sigs/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Sigs() {
+  return (
+    <main>
+      <h1 className="text-white">
+        HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO
+        HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO
+        HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO HELLO
+      </h1>
+    </main>
+  );
+}
