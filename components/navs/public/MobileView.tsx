ac085a71a04ab8b033395f86af8005f90150e847
devRaxx <rpcastaneros@up.edu.ph>
2025-03-18 16:45:33 +0800

    feat: initial commit + route setup

--- components/navs/public/MobileView.tsx
+++ components/navs/public/MobileView.tsx
@@ -95,7 +95,7 @@ export default function MobileView() {
             </Button> */}
             <Button
               variant={'navLinkMobile'}
-              onClick={() => redirectTo({ href: '/sigs' })}
+              onClick={() => redirectTo({ href: 'sigs' })}
             >
               Code of Conduct
             </Button>
