b8292c69c11d85158b9307eb1cc57dae3fb7fcaf
devRaxx <rpcastaneros@up.edu.ph>
2025-03-19 20:38:47 +0800

    feat: updated design up to ipad ver. on figma

--- app/sigs/page.tsx
+++ app/sigs/page.tsx
@@ -10,12 +10,12 @@ export default function Sigs() {
   return (
     <main className="flex flex-col justify-center mt-28">
       <div>
-        <p className="text-primary text-[32px] leading-9 font-black text-center">
+        <p className="text-primary text-[32px] md:text-[70px] leading-9 md:leading-none font-black text-center">
           SPECIAL <br />
           <span className="text-white">INTEREST</span> GROUPS
         </p>
 
-        <p className="text-white text-xs text-center mt-4 mx-auto w-[85%]">
+        <p className="text-white text-xs md:text-xl text-center mt-4 mx-auto w-[85%]">
           DurianPy&rsquo;s{' '}
           <span className="text-primary font-bold">
             Special Interest Groups
@@ -29,17 +29,17 @@ export default function Sigs() {
           collaboration.
         </p>
 
-        <button className="mt-4 mx-auto block bg-primary text-[8px] leading-3 font-bold rounded-xl px-2 py-1">
+        <button className="my-6 md:my-8 lg:my-10 mx-auto block bg-primary text-[8px] md:text-xl leading-3 md:leading-none font-bold rounded-xl px-2 md:px-4 lg:px-8 py-1 md:py-2 lg:py-4">
           Join a SIG today
         </button>
       </div>
 
-      <div className="mt-16">
-        <p className="text-primary text-[32px] leading-9 font-black text-center">
+      <div>
+        <p className="text-primary text-[32px] md:text-[70px] leading-9 md:leading-none font-black text-center">
           CURRENT <span className="text-white">SIGs</span>
         </p>
 
-        <div className="flex flex-wrap justify-center gap-6 mt-6">
+        <div className="flex flex-wrap justify-center gap-3 mt-6">
           {sigsData.map((sig, index) => (
             <Card key={index} image={sig.image} title={sig.title} />
           ))}
@@ -47,10 +47,10 @@ export default function Sigs() {
       </div>
 
       <div className="mt-16">
-        <p className="text-primary text-xl font-semibold text-center">
+        <p className="text-primary text-xl md:text-4xl font-semibold text-center">
           Want to Start a SIG?
         </p>
-        <p className="text-white text-xs text-center mt-4 mx-auto w-[85%]">
+        <p className="text-white text-xs md:text-2xl text-center mt-4 mx-auto w-[85%]">
           If you’re passionate about a particular Python topic and want to start
           a SIG, reach out to us at{' '}
           <span className="underline">durianpy.davao@gmail.com</span>. Our
