a1b6965f4ee61d71be820c301085d0ead911f44c
devRaxx <rpcastaneros@up.edu.ph>
2025-03-19 20:38:47 +0800

    feat: updated design up to ipad ver. on figma

--- app/sigs/components/card.tsx
+++ app/sigs/components/card.tsx
@@ -8,23 +8,24 @@ interface CardProps {
 
 export default function Card({ image, title }: CardProps) {
   return (
-    <div className="rounded-lg bg-gradient-to-b from-[rgba(62,179,114,0.2)] to-[rgba(25,113,64,0.2)] p-4 w-44 h-44 flex flex-col items-center justify-center border-t-2 border-midori-green">
-      {/* Image Rendering */}
-      <div className="text-yellow-400 mb-2">
+    <div
+      className="rounded-lg bg-gradient-to-b from-[rgba(62,179,114,0.2)] to-[rgba(25,113,64,0.2)] 
+                    w-[188px] md:w-[328px] h-[195px] md:h-[340px] 
+                    flex flex-col items-center justify-center border-t-2 border-midori-green"
+    >
+      <div className="relative w-16 h-16 md:w-40 md:h-40 mb-2">
         {typeof image === 'string' ? (
-          <Image src={image} alt={title} width={50} height={50} />
+          <Image src={image} alt={title} layout="fill" objectFit="contain" />
         ) : (
           image
         )}
       </div>
 
-      {/* Title */}
-      <h3 className="text-yellow-400 font-bold text-lg text-center mb-2">
+      <h3 className="text-yellow-400 font-bold text-lg md:text-[40px] md:leading-none text-center mb-2 md:mb-8">
         {title}
       </h3>
 
-      {/* Button */}
-      <button className="bg-yellow-400 text-xs text-black py-1 px-3 rounded-full font-medium hover:bg-yellow-300 transition-colors">
+      <button className="bg-yellow-400 text-xs text-black py-1 px-3 rounded-full font-bold">
         READ MORE
       </button>
     </div>
