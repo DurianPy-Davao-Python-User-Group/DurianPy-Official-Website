c0ea6ebd9da5b3acb714ba6ab41a2b98ce98342d
devRaxx <rpcastaneros@up.edu.ph>
2025-03-19 21:07:40 +0800

    feat: added button functionalities

--- app/sigs/components/card.tsx
+++ app/sigs/components/card.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import Image from 'next/image';
 
@@ -7,11 +10,13 @@ interface CardProps {
 }
 
 export default function Card({ image, title }: CardProps) {
+  const router = useRouter();
+
   return (
     <div
       className="rounded-lg bg-gradient-to-b from-[rgba(62,179,114,0.2)] to-[rgba(25,113,64,0.2)] 
                     w-[188px] md:w-[328px] h-[195px] md:h-[340px] 
-                    flex flex-col items-center justify-center border-t-2 border-midori-green"
+                    flex flex-col items-center justify-center border-t-2 border-midori-green hover:scale-105 duration-300"
     >
       <div className="relative w-16 h-16 md:w-40 md:h-40 mb-2">
         {typeof image === 'string' ? (
@@ -25,7 +30,10 @@ export default function Card({ image, title }: CardProps) {
         {title}
       </h3>
 
-      <button className="bg-yellow-400 text-xs text-black py-1 px-3 rounded-full font-bold">
+      <button
+        className="bg-yellow-400 text-xs text-black py-1 px-3 rounded-full font-bold hover:scale-110 duration-300"
+        onClick={() => router.push('/404')}
+      >
         READ MORE
       </button>
     </div>
