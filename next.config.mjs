6dd604f85251ff7754ca7abcd6b787ca803d2eb8
euxia. <denieldavenatividad@gmail.com>
2026-02-04 21:31:44 +0800

    fix: security headers, csp, nosniff. referrer and permissions policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   async headers() {
     return [
       {
@@ -10,9 +11,43 @@ const nextConfig = {
             value: 'max-age=31536000; includeSubDomains; preload',
           },
           {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
+              style-src 'self' 'unsafe-inline' https:;
+              img-src 'self' data: https: blob:;
+              font-src 'self' data: https:;
+              connect-src 'self' https:;
+              object-src 'none';
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'self';
+            `.replace(/\s+/g, ' ').trim(),
+          },
+          {
             key: 'x-frame-options',
             value: 'SAMEORIGIN',
           },
+          {
+            key: "x-content-type-options",
+            value: "nosniff",
+          },
+          {
+            key: "referrer-policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: `
+              camera=(),
+              microphone=(),
+              geolocation=(),
+              interest-cohort=(),
+              usb=(),
+              payment=()
+            `.replace(/\s+/g, " ").trim(),
+          },                          
         ],
       },
     ];
