fb77e91563f81b5c005b083eacd082526eb030d4
seangaaab <seangabbayron@gmail.com>
2026-07-15 23:07:20 +0800

    fix: update deploy assets script

--- scripts/deploy-assets.sh
+++ scripts/deploy-assets.sh
@@ -11,11 +11,8 @@ if [ -z "$CLOUDFRONT_DISTRIBUTION_ID" ]; then
   exit 1
 fi
 
-echo "Uploading immutable assets to S3 bucket: $S3_BUCKET"
-aws s3 sync .output/public/assets s3://${S3_BUCKET}/assets --delete --cache-control "public, max-age=31536000, immutable"
-
-echo "Uploading mutable public assets to S3 bucket: $S3_BUCKET"
-aws s3 sync .output/public s3://${S3_BUCKET}/ --delete --exclude "assets/*" --cache-control "public, max-age=3600"
+echo "Uploading public assets to S3 bucket: $S3_BUCKET"
+aws s3 sync .output/public s3://${S3_BUCKET}/ --delete --cache-control "public, max-age=3600"
 
 echo "Invalidating CloudFront cache for distribution: $CLOUDFRONT_DISTRIBUTION_ID"
 aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
